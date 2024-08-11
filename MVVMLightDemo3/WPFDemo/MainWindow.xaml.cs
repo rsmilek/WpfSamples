c52e39694420d55f9715b6d86089aa6e1078043e
Radim SMILEK <radim.smilek@cz.abb.com>
2024-08-11 18:23:58 +0200

    Static resources example: data & DataTemplate

--- MVVMLightDemo3/WPFDemo/MainWindow.xaml.cs
+++ MVVMLightDemo3/WPFDemo/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WPFDemo.Models;
 
 namespace WPFDemo
 {
@@ -20,9 +10,30 @@ namespace WPFDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<TodoItem> TodoItems { get; set; }
+
         public MainWindow()
         {
+            // Initialize the list with some data
+            TodoItems = new List<TodoItem>
+            {
+                new TodoItem { Name = "Item 1", Description = "Description 1", Priority = 1 },
+                new TodoItem { Name = "Item 2", Description = "Description 2", Priority = 2 },
+                new TodoItem { Name = "Item 3", Description = "Description 3", Priority = 3 }
+            };
+
+            // Register the list as a resource
+            // ATTENSION:
+            // Resource must be registered before InitializeComponent() method is called!!!
+            // This is not the best practice, but it is a simple way to pass data to the view
+            this.Resources["TodoItemsCodeBehind"] = TodoItems;
+
+
             InitializeComponent();
         }
+    }
+
+    public class TodoItemCollection : ObservableCollection<TodoItem>
+    {
     }
 }
---
+++ MVVMLightDemo3/WPFDemo/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WPFDemo.Models
+{
+    public class TodoItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+    }
+}

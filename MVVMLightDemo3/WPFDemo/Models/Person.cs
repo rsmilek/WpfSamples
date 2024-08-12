9e16f601602adb806e13a5a20ed74824251cec59
Radim SMILEK <radim.smilek@cz.abb.com>
2024-08-12 15:08:50 +0200

    Data Biding example -> UpdateSourceTrigger

---
+++ MVVMLightDemo3/WPFDemo/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
